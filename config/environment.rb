af203b54a34613a7cf016325047c0843789c1f10
Matthew Bunday <zenceph@gmail.com>
2015-04-25 21:26:08 -0400

    Add instagram setup to environment

--- config/environment.rb
+++ config/environment.rb
@@ -20,6 +20,17 @@ require "sinatra/reloader" if development?
 
 require 'erb'
 
+
+CALLBACK_URL = "http://localhost:9393/oauth/callback"
+
+Instagram.configure do |config|
+  config.client_id = "YOUR_CLIENT_ID"
+  config.client_secret = "YOUR_CLIENT_SECRET"
+  # For secured endpoints only
+  #config.client_ips = '<Comma separated list of IPs>'
+end
+
+
 # Some helper constants for path-centric logic
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
 
