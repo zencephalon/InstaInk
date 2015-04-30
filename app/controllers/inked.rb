18ad9274e30eec38e00f042e0f06e87b2d8c760f
Matthew Bunday <zenceph@gmail.com>
2015-04-30 15:54:03 -0400

    Working on AJAX call for photos

--- app/controllers/inked.rb
+++ app/controllers/inked.rb
@@ -8,6 +8,8 @@ end
 get '/user/photos' do
   client = instaclient
 
+  params[:next_max_id] = nil if params[:next_max_id] == "false"
+
   media = client.user_recent_media({max_id: params[:next_max_id]})
   return {photos: media, next_max_id: media.pagination.next_max_id}.to_json
 end
--- public/js/gallery.js
+++ public/js/gallery.js
@@ -8,25 +8,30 @@ function Photo(data, gallery) {
 
 function Gallery($ele) {
   this.$ele = $ele;
-  this.lastPhotoID = false;
+  this.next_max_id = false;
+  this.$spinner = $('<img class="center" style="display:block" src="/img/spinner-small.gif">');
+  this.$ele.append(this.$spinner);
+  this.$spinner.hide();
 
   this.loadMorePhotos();
 }
 
 Gallery.prototype.loadMorePhotos = function() {
   var self = this;
-  if (!this.lastPhotoID) {
-    $.ajax({
-      url: '/user/photos',
-      type: 'GET',
-      dataType: 'JSON'
-    }).done(function(response) {
-      console.log(response);
-      response.photos.forEach(function (photo) {
-        var photo = new Photo(photo, self);
-      })
+  this.$spinner.show();
+  $.ajax({
+    url: '/user/photos',
+    type: 'GET',
+    data: {next_max_id: self.next_max_id},
+    dataType: 'JSON'
+  }).done(function(response) {
+    console.log(response);
+    self.$spinner.hide();
+    self.next_max_id = response.next_max_id;
+    response.photos.forEach(function (photo) {
+      var photo = new Photo(photo, self);
     })
-  }
+  })
 }
 
 
