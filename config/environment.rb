eabafeb1f1b3b8ba0f82785dc51f0f842dc135e1
Matthew Bunday <zenceph@gmail.com>
2015-04-30 14:47:59 -0400

    Configure instagram client

--- config/environment.rb
+++ config/environment.rb
@@ -20,12 +20,18 @@ require "sinatra/reloader" if development?
 
 require 'erb'
 
+require 'pry'
+
+require 'dotenv'
+Dotenv.load
+
+require 'instagram'
 
 CALLBACK_URL = "http://localhost:9393/oauth/callback"
 
 Instagram.configure do |config|
-  config.client_id = "YOUR_CLIENT_ID"
-  config.client_secret = "YOUR_CLIENT_SECRET"
+  config.client_id = ENV['CLIENT_ID']
+  config.client_secret = ENV['CLIENT_SECRET']
   # For secured endpoints only
   #config.client_ips = '<Comma separated list of IPs>'
 end
