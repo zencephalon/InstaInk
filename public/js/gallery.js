66dbae4ea3e26d3e21ac5785c41c9c9a0d145d6e
Matthew Bunday <zenceph@gmail.com>
2015-04-30 14:59:04 -0400

    Setup JS gallery controller

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -1,3 +1,23 @@
+function Gallery($ele) {
+  this.$ele = $ele;
+  this.lastPhotoID = false;
+
+  this.loadMorePhotos();
+}
+
+Gallery.prototype.loadMorePhotos = function() {
+  if (!this.lastPhotoID) {
+    $.ajax({
+      url: '/user/photos',
+      type: 'GET',
+      datatype: 'JSON'
+    }).done(function(response) {
+      console.log(response);
+    })
+  }
+}
+
+
 $(document).ready(function() {
-  
+  var gallery = new Gallery($('#gallery'));
 })
