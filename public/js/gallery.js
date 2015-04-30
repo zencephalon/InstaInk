97a86e700c06830ecc6deebc86df084b0e464331
Matthew Bunday <zenceph@gmail.com>
2015-04-30 16:02:19 -0400

    Load more images on scroll bottom

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -9,32 +9,41 @@ function Photo(data, gallery) {
 function Gallery($ele) {
   this.$ele = $ele;
   this.next_max_id = false;
-  this.$spinner = $('<img class="center" style="display:block" src="/img/spinner-small.gif">');
-  this.$ele.append(this.$spinner);
-  this.$spinner.hide();
-
+  this.$spinner = $('<img id="spinner" class="center" style="display:block" src="/img/spinner-small.gif">');
+  this.photosLoaded = true;
   this.loadMorePhotos();
 }
 
 Gallery.prototype.loadMorePhotos = function() {
-  var self = this;
-  this.$spinner.show();
-  $.ajax({
-    url: '/user/photos',
-    type: 'GET',
-    data: {next_max_id: self.next_max_id},
-    dataType: 'JSON'
-  }).done(function(response) {
-    console.log(response);
-    self.$spinner.hide();
-    self.next_max_id = response.next_max_id;
-    response.photos.forEach(function (photo) {
-      var photo = new Photo(photo, self);
+  if (this.photosLoaded) {
+    this.photosLoaded = false;
+    var self = this;
+    this.$ele.append(this.$spinner);
+    $.ajax({
+      url: '/user/photos',
+      type: 'GET',
+      data: {next_max_id: self.next_max_id},
+      dataType: 'JSON'
+    }).done(function(response) {
+      console.log(response);
+      self.next_max_id = response.next_max_id;
+      self.photosLoaded = true;
+      $('#spinner').remove();
+      response.photos.forEach(function (photo) {
+        var photo = new Photo(photo, self);
+      })
     })
-  })
+  }
 }
 
 
 $(document).ready(function() {
   var gallery = new Gallery($('#gallery'));
+
+  $(window).scroll(function() {
+    if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+      console.log("Bottom!")
+      gallery.loadMorePhotos();
+    }
+  });
 })
