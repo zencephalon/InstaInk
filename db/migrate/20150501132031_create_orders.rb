35617be1b689a4b457a7965625d945835c58b1e1
Matthew Bunday <zenceph@gmail.com>
2015-05-01 13:35:11 -0400

    Add migrations for photos and orders.

---
+++ db/migrate/20150501132031_create_orders.rb
@@ -0,0 +1,20 @@
+class CreateOrders < ActiveRecord::Migration
+  def change
+    create_table :orders do |t|
+      t.string :stripeToken
+      t.string :stripeEmail
+      t.string :stripeShippingName
+      t.string :stripeShippingAddressLine1
+      t.string :stripeShippingAddressApt
+      t.string :stripeShippingAddressZip
+      t.string :stripeShippingAddressCity
+      t.string :stripeShippingAddressState
+      t.string :stripeShippingAddressCountry
+      t.string :stripeShippingAddressCountryCode
+
+      t.boolean :paid
+
+      t.timestamps
+    end
+  end
+end
