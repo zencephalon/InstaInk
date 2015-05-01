2f713ae4f6141ed0e8ffc3b13b33347fb95434b1
Matthew Bunday <zenceph@gmail.com>
2015-05-01 14:33:32 -0400

    Working on payment logic.

--- app/controllers/order.rb
+++ app/controllers/order.rb
@@ -8,6 +8,19 @@ post '/order' do
   "/order/#{order.id}"
 end
 
+post '/order/:id/paid' do |id|
+  order = Order.find(id)
+  useful_params = {}
+  %w(stripeToken stripeEmail stripeShippingName stripeShippingAddressLine1 stripeShippingAddressApt stripeShippingAddressZip stripeShippingAddressCity stripeShippingAddressState stripeShippingAddressCountry stripeShippingAddressCountryCode).each do |attr|
+    useful_params[attr] = params[attr]
+  end
+  order.update_attributes(useful_params)
+
+  
+  
+  erb :'order/paid'
+end
+
 get '/order/:id' do |id|
   @order = Order.find(id)
   erb :'order/show'
