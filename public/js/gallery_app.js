a115c7efd050fd40ca677eb4c01d0edd06196c4d
Matthew Bunday <zenceph@gmail.com>
2015-06-14 13:29:31 -0400

    Fix the scroll issue

--- public/js/gallery_app.js
+++ public/js/gallery_app.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
   gallery = new Gallery($('#gallery'));
 
+  setInterval(function() {
+    if($(window).height() >= $(document).height()){
+      gallery.loadMorePhotos();
+    };
+  }, 300);
+
   $(window).scroll(function() {
     if($(window).scrollTop() + $(window).height() >= $(document).height() - 100) {
       console.log("Bottom!")
